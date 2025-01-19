b501323768176fd4dd15c421374e33ae0bb685d4
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 15:26:12 -0800

    gets interest rate

--- Wk1Ex3/Program.cs
+++ Wk1Ex3/Program.cs
@@ -3,3 +3,7 @@
 Console.WriteLine("What is the principal amount of the laon in united states dollars?");
 
 string principal = Console.ReadLine();
+
+Console.WriteLine("What is the interest rate of the lona (input as decmial 00.5 for 5%)?");
+
+string interestrate = Console.ReadLine();
