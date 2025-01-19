60a7b51d49bc0b69a703607ea7bb483253a5664e
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 15:24:39 -0800

    gets principal amount of loan

--- Wk1Ex3/Program.cs
+++ Wk1Ex3/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("What is the principal amount of the laon in united states dollars?");
+
+string principal = Console.ReadLine();
