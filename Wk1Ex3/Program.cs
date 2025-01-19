0a301ea5995c1f5b791a549512cb10d7ee9b10e4
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 15:44:32 -0800

    written lines work

--- Wk1Ex3/Program.cs
+++ Wk1Ex3/Program.cs
@@ -2,8 +2,14 @@
 
 Console.WriteLine("What is the principal amount of the laon in united states dollars?");
 
-string principal = Console.ReadLine();
+
 
 Console.WriteLine("What is the interest rate of the lona (input as decmial 00.5 for 5%)?");
 
-string interestrate = Console.ReadLine();
+
+
+Console.WriteLine("What is the period/lenght of the loan in years");
+
+
+
+Console.WriteLine(" Total interest of the Loan: " + " USD. ");
