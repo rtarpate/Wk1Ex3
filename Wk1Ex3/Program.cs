657eeba8acc30817a625d94661f93c6bbddb0a1a
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 16:01:11 -0800

    code works and caulates the interest

--- Wk1Ex3/Program.cs
+++ Wk1Ex3/Program.cs
@@ -2,14 +2,15 @@
 
 Console.WriteLine("What is the principal amount of the laon in united states dollars?");
 
-
+int principal = int.Parse(Console.ReadLine());
 
 Console.WriteLine("What is the interest rate of the lona (input as decmial 00.5 for 5%)?");
 
+double interestrate = double.Parse(Console.ReadLine());
 
 
 Console.WriteLine("What is the period/lenght of the loan in years");
 
+int period = int.Parse(Console.ReadLine());
 
-
-Console.WriteLine(" Total interest of the Loan: " + " USD. ");
+Console.WriteLine(" Total interest of the Loan: " + principal*interestrate*period + " USD. ");
